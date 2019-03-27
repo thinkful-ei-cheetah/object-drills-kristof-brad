bf4134dc5f4ac88f827751d36a21021195122419
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-03-27 11:16:51 -0500

    completed first drill

--- object-drills-kristof-brad/objectDrills3.js
+++ object-drills-kristof-brad/objectDrills3.js
@@ -7,6 +7,16 @@
 // Fill in the body of the method to return the hydration of the loaf (the water divided by the flour multiplied by 100).
 // Call the hydration method and use console.log to print the result.
 
+const loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function (){ 
+    return this.water / this.flour * 100;
+  },
+};
+
+console.log(loaf.flour, loaf.water);
+console.log(loaf.hydration());
 
 
 
