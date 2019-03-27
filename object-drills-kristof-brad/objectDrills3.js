f5afb292149df43b31c5a8d9d8b61db0a233cf35
Brad Schilit <bradleyschilit@mac.com>
2019-03-27 12:23:12 -0400

    2 done

--- object-drills-kristof-brad/objectDrills3.js
+++ object-drills-kristof-brad/objectDrills3.js
@@ -24,3 +24,44 @@ console.log(loaf.hydration());
 // Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
 // Loop over the object using for ... in
 // Use console.log to show each property name and its associated value.
+
+cont myObj = {
+    foo: 2
+    bar: 3
+    fum: 6
+    quux: 9
+    spam: 7
+}
+
+for (let key in myObj) {
+    console.log(key, myObj[key])
+}
+
+
+
+// 3. Arrays in objects
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+
+
+
+
+
+// 4. Arrays of objects
+// Create 3-5 objects, each with a name and a jobTitle. Use people you know, or characters from fiction, or your own inventions.
+// Store these objects in an array.
+// Iterate over the array and use console.log to show each person's job title and name.
+
+
+
+
+
+
+
+// 5. Properties that aren't there
+// Expand on the previous example by adding a boss property to everyone except the owner of the company.
+// Change the iteration to print out messages in this format: "${title} ${name} reports to ${boss}.". For example: Junior Engineer Bob reports to Fred..
+// What gets printed out for the owner?
+// Adjust the message so that people with no boss display "${title} ${name} doesn't report to anybody." - for example, Founder John doesn't report to anybody.
